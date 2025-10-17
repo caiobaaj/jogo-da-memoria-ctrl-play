9809f1536ad9d767104137326d24b73ca0648cd5
Caio Teodoro <caioteodoro2012@gmail.com>
2025-10-17 18:00:45 -0300

    adição das funções card e board

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
+function urlBuilder(){}
+
+let card = new CardManager(urlBuilder);
+let board = new BoardManager("board", 50, card);
+
 let menu = document.getElementById('menu');
 let select = document.getElementById('numCards');
 let start = document.getElementById('start');
@@ -13,3 +18,11 @@ for (let i = 4; i < 11; i+=2) {
     select.appendChild(opção);
 }
 
+start.addEventListener('click', () => {
+    menu.classList.add('hidden');
+    board.node.classList.remove('hidden');
+    board.fill(select.value);
+});
+
+//comando do desenvolvedor
+start.click();
